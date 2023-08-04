2093232074eb3616fedb9592bc50f60d8f17b6df
Teimurazi <teimurazi.toloraia@vaticle.com>
2023-08-04 17:56:37 +0400

    some progress in rust code

--- checkstyle/src/main.rs
+++ checkstyle/src/main.rs
@@ -1,21 +1,37 @@
+use regex::Regex;
 use std::fs::{self};
 use text_io::read;
-use regex::Regex;
-
+// use std::path::Path;
+// use std::path::PathBuf;
+// use std::fs::File;
+// use std::io::prelude::*;
 
 fn get_file(filename: &str) -> String {
     fs::read_to_string(filename).expect("Failed to read schema file")
 }
 
-fn check_matching(file: &str, regex: &str) -> bool {
-    let rx = Regex::new(regex).unwrap();
-    true
-    // TODO
+fn check_matching(file: &str, regex: &str) -> Option<bool> // typedb_client::Result<bool> 
+{
+    // true
+    let re = Regex::new(regex)?;
+    Ok(re.is_match(file))
 }
 
-fn all_files(target_path: &str) -> Vec<&str> {
-    vec![]
-    // TODO
+fn all_files(dir: &str) -> Vec<&str> {
+    let mut files: Vec<&str> = Vec::new();
+    for file in dir {
+        if let Ok(file) = file {
+            if file.file_type().unwrap().is_file() {
+                files.push(file.path());
+            } else if file.file_type().unwrap().is_dir() {
+                // Extract all files, even from directories.
+                // Doesn't follow symlinks.
+                let rdir = file.path().read_dir().unwrap();
+                files.append(&mut files_from_dir(rdir));
+            }
+        }
+    }
+    files
 }
 
 fn solve(regex_file_path: &str, target_path: &str) -> (Vec<String>, Vec<String>) {
@@ -26,7 +42,8 @@ fn solve(regex_file_path: &str, target_path: &str) -> (Vec<String>, Vec<String>)
 
     for target_file_name in target_file_paths {
         let target_file = get_file(&target_file_name);
-        if check_matching(&target_file, &regex) {
+        let is_match: bool = check_matching(&target_file, &regex);
+        if is_match {
             matching_files.push(target_file_name.to_string());
         } else {
             nonmatching_files.push(target_file_name.to_string());
@@ -35,7 +52,6 @@ fn solve(regex_file_path: &str, target_path: &str) -> (Vec<String>, Vec<String>)
 
     (matching_files, nonmatching_files)
 }
-
 
 fn main() {
     print!("Type regex file path");
