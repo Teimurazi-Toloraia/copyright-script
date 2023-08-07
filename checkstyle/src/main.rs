d3d8eb527ee22011bb9c464021fafa55c01eae13
Teimurazi <teimurazi.toloraia@vaticle.com>
2023-08-07 15:30:46 +0400

    fixed check_matching

--- checkstyle/src/main.rs
+++ checkstyle/src/main.rs
@@ -10,11 +10,9 @@ fn get_file(filename: &str) -> String {
     fs::read_to_string(filename).expect("Failed to read schema file")
 }
 
-fn check_matching(file: &str, regex: &str) -> Option<bool> // typedb_client::Result<bool> 
-{
-    // true
-    let re = Regex::new(regex)?;
-    Ok(re.is_match(file))
+fn check_matching(file: &str, regex: &str) -> Option<bool> {
+    let re = Regex::new(regex).ok()?; // Use .ok() to convert Result to Option
+    Some(re.is_match(file))
 }
 
 fn all_files(dir: &str) -> Vec<&str> {
@@ -42,7 +40,11 @@ fn solve(regex_file_path: &str, target_path: &str) -> (Vec<String>, Vec<String>)
 
     for target_file_name in target_file_paths {
         let target_file = get_file(&target_file_name);
-        let is_match: bool = check_matching(&target_file, &regex);
+        let is_match_option = check_matching(&target_file, &regex);
+        let is_match: bool = match is_match_option {
+            Some(inner_value) => inner_value,
+            None => false,
+        };
         if is_match {
             matching_files.push(target_file_name.to_string());
         } else {
