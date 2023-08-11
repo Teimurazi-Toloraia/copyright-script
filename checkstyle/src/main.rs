0feff8ebca1d68368f199aea4e0db15a7294d057
Teimurazi <teimurazi.toloraia@vaticle.com>
2023-08-11 18:19:26 +0400

    code review

--- checkstyle/src/main.rs
+++ checkstyle/src/main.rs
@@ -1,29 +1,23 @@
 use regex::Regex;
-use std::fs::{self};
+use std::fs;
 
-fn get_file(filename: &str) -> String {
-    fs::read_to_string(filename).expect("Failed to read file")
+fn read_file_contents(file_path: &str) -> String {
+    fs::read_to_string(file_path).expect("Failed to read file")
 }
 
-fn split_into_lines(s: &str) -> Vec<&str> {
-    s.lines().collect()
-}
-
-fn check_matching_regex_line(file: &str, re: Regex) -> Option<bool> {
-    let file_lines = split_into_lines(file);
-    for file_line in file_lines {
+fn check_matching_regex_line(file: &str, re: Regex) -> bool {
+    for file_line in file.lines() {
         if re.is_match(file_line) {
-            return Some(true);
+            return true;
         }
     }
-    Some(false)
+    false
 }
 
 fn check_matching(file: &str, regex: &str) -> Option<bool> {
-    let lines: Vec<&str> = split_into_lines(regex);
-    for regex_line in lines {
+    for regex_line in regex.lines() {
         let re = Regex::new(regex_line).ok()?;
-        if check_matching_regex_line(file, re) == Some(false) {
+        if !check_matching_regex_line(file, re) {
             return Some(false);
         }
     }
@@ -50,24 +44,17 @@ fn list_files_in_folder(folder_path: &str) -> Result<Vec<String>, std::io::Error
 }
 
 fn all_files(folder_path: &str) -> Vec<String> {
-    match list_files_in_folder(folder_path) {
-        Ok(file_paths) => {
-            return file_paths;
-        }
-        _ => {
-            return Vec::new();
-        }
-    }
+    list_files_in_folder(folder_path).unwrap_or_default()
 }
 
 fn solve(regex_file_path: &str, target_path: &str) -> (Vec<String>, Vec<String>) {
     let target_file_paths = all_files(target_path);
-    let regex = get_file(regex_file_path);
+    let regex = read_file_contents(regex_file_path);
     let mut matching_files: Vec<String> = Vec::new();
     let mut nonmatching_files: Vec<String> = Vec::new();
 
     for target_file_name in target_file_paths {
-        let target_file = get_file(&target_file_name);
+        let target_file = read_file_contents(&target_file_name);
         let is_match_option = check_matching(&target_file, &regex);
         let is_match: bool = match is_match_option {
             Some(inner_value) => inner_value,
@@ -90,32 +77,28 @@ fn main() {
     let target_path: String = ".".to_string();
     let (matching_files, nonmatching_files) = solve(&regex_file_path, &target_path);
     for file in &matching_files {
-        println!("{file} is a matching file");
+        println!("{file} is matching regex in {regex_file_path}");
     }
     for file in &nonmatching_files {
-        println!("{file} is a nonmatching file");
+        println!("{file} is NOT matching regex in {regex_file_path}");
     }
-    // Check if there are non-matching files and return the appropriate exit code
     if !nonmatching_files.is_empty() {
-        process::exit(1); // Exit with code 1 for non-matching files
+        process::exit(1);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::check_matching;
-    use crate::get_file;
+    use crate::read_file_contents;
 
     fn is_option_true(option_value: Option<bool>) -> bool {
-        match option_value {
-            Some(true) => true,
-            _ => false,
-        }
+        option_value.unwrap_or(false)
     }
     #[test]
     fn test_checker() {
-        let regex = get_file("checkstyle-file-agpl-header.txt");
-        let file = get_file("Syncer.kt");
+        let regex = read_file_contents("checkstyle-file-agpl-header.txt");
+        let file = read_file_contents("Syncer.kt");
         println!("regex is {regex}");
         println!("file is {file}");
         let result = check_matching(&file, &regex);
